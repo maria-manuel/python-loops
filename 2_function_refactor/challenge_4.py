d74af7a9f5969a97487e41cda169f31496afe0d3
maria-manuel <mariamanuel.tech@gmail.com>
2023-09-27 16:17:22 -0700

    refractor def integer_value for costume measurements

--- 2_function_refactor/challenge_4.py
+++ 2_function_refactor/challenge_4.py
@@ -2,49 +2,99 @@
 
 
 
-def main():
-    print('Cosplay time! Enter measurements for costume.')
+# def main():
+#     print('Cosplay time! Enter measurements for costume.')
 
-    print('Enter the bust or chest measurement:')
+#     print('Enter the bust or chest measurement:')
 
+#     value = input('? ')
+#     while not value.isnumeric():
+#         print('Please enter an integer number.')
+#         value = input('? ')
+#     integer_value = int(value)
+
+#     bust = integer_value
+
+
+#     print('Enter the waist measurement:')
+
+#     value = input('? ')
+#     while not value.isnumeric():
+#         print('Please enter an integer number.')
+#         value = input('? ')
+#     integer_value = int(value)
+
+#     waist = integer_value
+
+
+#     print('Enter the hip measurement:')
+
+#     value = input('? ')
+#     while not value.isnumeric():
+#         print('Please enter an integer number.')
+#         value = input('? ')
+#     integer_value = int(value)
+
+#     hips = integer_value
+
+
+#     print('Enter the height (hollow to hem) measurement:')
+
+#     value = input('? ')
+#     while not value.isnumeric():
+#         print('Please enter an integer number.')
+#         value = input('? ')
+#     integer_value = int(value)
+
+#     height = integer_value
+
+
+#     print('----------------')
+#     print('Great job! Your measurements are:')
+#     print('Bust:', bust)
+#     print('Waist:', waist)
+#     print('Hips:', hips)
+#     print('Height:', height)
+#     print('Have fun cosplaying!')
+
+
+# main()
+
+def get_measurement():
     value = input('? ')
     while not value.isnumeric():
         print('Please enter an integer number.')
         value = input('? ')
     integer_value = int(value)
+    return integer_value
+
+def main():
+    print('Cosplay time! Enter measurements for costume.')
+
+    print('Enter the bust or chest measurement:')
+
+    integer_value = get_measurement()
 
     bust = integer_value
 
 
     print('Enter the waist measurement:')
 
-    value = input('? ')
-    while not value.isnumeric():
-        print('Please enter an integer number.')
-        value = input('? ')
-    integer_value = int(value)
+    integer_value=get_measurement()
 
     waist = integer_value
 
 
     print('Enter the hip measurement:')
 
-    value = input('? ')
-    while not value.isnumeric():
-        print('Please enter an integer number.')
-        value = input('? ')
-    integer_value = int(value)
+    integer_value=get_measurement()
 
     hips = integer_value
 
 
     print('Enter the height (hollow to hem) measurement:')
 
-    value = input('? ')
-    while not value.isnumeric():
-        print('Please enter an integer number.')
-        value = input('? ')
-    integer_value = int(value)
+    integer_value=get_measurement()
 
     height = integer_value
 
@@ -59,4 +109,3 @@ def main():
 
 
 main()
-
